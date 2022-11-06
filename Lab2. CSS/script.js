fb430eed8fc55f28b5bd814d96be6385900c1c7d
Fedor Rudnik <63184742+FedQa@users.noreply.github.com>
2022-11-06 20:51:17 +0500

    Add files via upload

---
+++ Lab2. CSS/script.js
@@ -0,0 +1,11 @@
+let phrases = [
+    '1.',
+    '2.',
+    '3.',
+    '4.',
+    '5.',
+    '6.'
+];
+let randIndex = Math.floor(Math.random() * phrases.length)
+console.log(randIndex);
+console.log(phrases[randIndex]);
